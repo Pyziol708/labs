3d9a82412ab59b5b5075b85a897f70675516fe78
Pyziol708 <k.standarski@gmail.com>
2016-10-19 17:03:11 +0200

    00017 zrobione

---
+++ 00017/Konto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00017
+{
+    public class Konto
+    {
+        private readonly uint id;
+        private double saldoPoczatkowe;
+        private double saldoKoncowe;
+
+        public Konto(uint aId, double aSaldoPoczatkowe)
+        {
+            id = aId;
+            saldoPoczatkowe = aSaldoPoczatkowe;
+            saldoKoncowe = saldoPoczatkowe;
+        }
+
+        public bool WykonajPrzelew(Konto konto, double kwota)
+        {
+            if (kwota < 0 || this.saldoKoncowe < kwota)
+                return false;
+
+            this.saldoKoncowe -= kwota;
+            konto.saldoKoncowe += kwota;
+            return true;
+        }
+
+        public bool WykonajPrzelew(double kwota)
+        {
+            if (kwota < 0 && saldoKoncowe < kwota)
+                return false;
+
+            saldoKoncowe -= kwota;
+            return true;
+        }
+
+        public bool DokonajWplaty(double kwota)
+        {
+            if (kwota < 0)
+                return false;
+            saldoKoncowe += kwota;
+            return true;
+        }
+
+        public bool DokonajWyplaty(double kwota)
+        {
+            if (kwota < 0 || saldoKoncowe < kwota)
+                return false;
+            saldoKoncowe -= kwota;
+            return true;
+        }
+
+        public void PokazZestawienie()
+        {
+            Console.Write("Konto [");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(id);
+            Console.ResetColor();
+            Console.WriteLine("]");
+            Console.WriteLine("Saldo poczatkowe: " + saldoPoczatkowe);
+            Console.WriteLine("Saldo koncowe: " + saldoKoncowe);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 00017/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Konto konto1 = new Konto(1, 1200);
+            Konto konto2 = new Konto(2, 5000);
+            Konto konto3 = new Konto(3, 0);
+
+            konto1.DokonajWplaty(100);
+            konto1.WykonajPrzelew(konto2, 1300);
+            konto2.WykonajPrzelew(konto3, 5000);
+            konto2.WykonajPrzelew(konto3, 200);
+            konto2.WykonajPrzelew(1000);
+
+            konto1.PokazZestawienie();
+            konto2.PokazZestawienie();
+            konto3.PokazZestawienie();
+            Console.ReadKey();
+        }
+    }
+}
