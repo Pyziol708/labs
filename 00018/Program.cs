a39c61890585d27b4da16cb8835de45df04f224b
Pyziol708 <k.standarski@gmail.com>
2016-10-19 17:39:18 +0200

    00018 zrobione

---
+++ 00018/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00018
+{
+    class Program
+    {
+        static Dictionary<uint, Telefon> telefony = new Dictionary<uint, Telefon>();
+        static void Main(string[] args)
+        {
+            telefony.Add(751345112, new Telefon(751345112, 0));
+            telefony.Add(751798234, new Telefon(751798234, 100));
+            telefony.Add(751987984, new Telefon(751987984, 150));
+
+            telefony[751345112].Doladowanie(50);
+            telefony[751987984].Internet(200);
+            telefony[751798234].SMS();
+            telefony[751798234].Rozmowa(31);
+            telefony[751345112].UslugaPremium(3.68);
+
+            foreach (uint key in telefony.Keys)
+            {
+                telefony[key].PokazZestawienie();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 00018/Telefon.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00018
+{
+    public class Telefon
+    {
+        private readonly uint numer;
+        private double saldoPoczatkowe;
+        private double saldoKoncowe;
+        public static double StawkaSMS = 0.20;
+        public static double StawkaImpuls = 0.40;
+        public static double StawkaGB = 30;
+
+        public Telefon(uint aNumer, double aSaldoPoczatkowe)
+        {
+            numer = aNumer;
+            saldoPoczatkowe = aSaldoPoczatkowe;
+            saldoKoncowe = saldoPoczatkowe;
+        }
+
+        public bool Doladowanie(double kwota)
+        {
+            if (kwota < 0)
+                return false;
+            saldoKoncowe += kwota;
+            return true;
+        }
+
+        public bool SMS()
+        {
+            if (StawkaSMS < 0 || saldoKoncowe < StawkaSMS)
+                return false;
+            saldoKoncowe -= StawkaSMS;
+            return true;
+        }
+
+        public bool Rozmowa(uint dlugosc)
+        {
+            if (StawkaImpuls < 0 || saldoKoncowe < StawkaImpuls * dlugosc)
+                return false;
+            saldoKoncowe -= StawkaImpuls * dlugosc;
+            return true;
+        }
+
+        public bool Internet(double MB)
+        {
+            if (MB < 0)
+                return false;
+
+            double naleznosc = MB * (StawkaGB / 1000);
+            if (saldoKoncowe < naleznosc)
+                return false;
+            saldoKoncowe -= naleznosc;
+            return true;
+        }
+
+        public bool UslugaPremium(double kwota)
+        {
+            if (kwota < 0 || saldoKoncowe < kwota)
+                return false;
+            saldoKoncowe -= kwota;
+            return true;
+        }
+
+        public void PokazZestawienie()
+        {
+            Console.Write("Numer [");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(String.Format("{0:+48 ### ### ###}", numer));
+            Console.ResetColor();
+            Console.WriteLine("]");
+            Console.WriteLine("Saldo poczatkowe: " + saldoPoczatkowe);
+            Console.WriteLine("Saldo koncowe: " + saldoKoncowe);
+            Console.WriteLine();
+        }
+    }
+}
